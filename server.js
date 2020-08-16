db50cc16d1fbe356f2c8f295218d2b18fd54fdcb
Dan Mulvihillgit config --global user.name danMulvihillgit config --global user.name Dan <dmulvihill3@gmail.com>
2020-08-16 19:13:29 -0400

    arrow functions are great

--- server.js
+++ server.js
@@ -6,6 +6,4 @@ app.get("/", (req,res) => res.send("<h1>Welcome</h1>") )
 
 app.get("/mars", (req,res) => res.send('<H2 style="color: red">Welcome to the Red Planet</H2>'))
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000")
-});
+app.listen(3000, () => console.log("Server started on port 3000") );
