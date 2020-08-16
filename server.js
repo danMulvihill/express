462d4599abe9341b447ddef648b41b5c2ced7802
Dan Mulvihillgit config --global user.name danMulvihillgit config --global user.name Dan <dmulvihill3@gmail.com>
2020-08-16 19:11:21 -0400

    added a route

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.get("/", (req,res) => res.send("<h1>Welcome</h1>") )
 
+app.get("/mars", (req,res) => res.send('<H2 style="color: red">Welcome to the Red Planet</H2>'))
+
 app.listen(3000, function(){
     console.log("Server started on port 3000")
 });
