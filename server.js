9f84a98ac51ddf1aa3bdd163746ab9a40ce39055
Dan Mulvihillgit config --global user.name danMulvihillgit config --global user.name Dan <dmulvihill3@gmail.com>
2020-08-16 21:01:45 -0400

    added a POST

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname+"/index.html") 
     console.log(__dirname)
+})
+
+app.post("/", function(req,res){
+
+    let sum = parseFloat(req.body.num1) + parseFloat(req.body.num2);
+    res.send(req.body.num1+" + "+req.body.num2+" = "+sum)
 })
 
 app.get("/mars", (req,res) => res.send('<H2 style="color: red">Welcome to the Red Planet</H2>'))
