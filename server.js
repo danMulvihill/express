1a57045090e2f0a8c6d378e5b633dabbe7273185
Dan Mulvihillgit config --global user.name danMulvihillgit config --global user.name Dan <dmulvihill3@gmail.com>
2020-08-16 20:33:31 -0400

    added a file

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req,res) => res.send("<h1>Welcome</h1>") )
+app.get("/", (req,res) => {
+    res.sendFile(__dirname+"/index.html") 
+    console.log(__dirname)
+})
 
 app.get("/mars", (req,res) => res.send('<H2 style="color: red">Welcome to the Red Planet</H2>'))
+
+app.get("/jupiter", (q,s) => s.send("<h1 style='font-size: 100px'>Welcome to Jupiter</h1>"))
+
+app.get("/calc"), function(q,s) {
+    s.sendFile(__dirname + "/calc.html");
+    console.log(__dirname)
+}
 
 app.listen(3000, () => console.log("Server started on port 3000") );
